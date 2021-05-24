9938374f9f22ce403a7a30ee8a4b2270e85fde1d
Andrew McIndoe <amcindoe@powerhrg.com>
2021-05-24 13:47:27 -0400

    Array Review Lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(){
+    return destructivelyAppendCat = cats.push('Ralph')
+}
+
+function destructivelyPrependCat(){
+    return destructivelyPrependCat = cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat(){
+    return destructivelyRemoveLastCat = cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return destructivelyRemoveFirstCat = cats.shift()
+}
+
+function appendCat(){
+    return appendCat = cats.concat('Broom')
+}
+
+function prependCat(){
+    return prependCat = ['Arnold',...cats]
+}
+
+function removeLastCat(){
+    return removeLastCat = cats.slice(0, 2)
+}
+
+function removeFirstCat(){
+    return removeFirstCat = cats.slice(1)
+}
